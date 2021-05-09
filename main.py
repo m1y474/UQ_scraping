f308b9816837afc34f98152c7392e034e3160cae
rina.miyata <rina.miyata@eilsystem.com>
2021-05-09 19:50:52 +0900

    fix: change the order functions defined

--- main.py
+++ main.py
@@ -8,42 +8,12 @@ import requests
 url = 'https://www.uqwimax.jp/mobile/products/sim/devices/'
 result = 'TORQUE 5GはまだUQモバイルの対象機種ではありません。'
 
-# ステータスコードの確認
-if requests.get(url).status_code != 200:
-    result = 'スクレイピング対象サイトのレスポンスが変更されました。対象サイトを確認してください。\n{}'.format(url)
-    send_email()
-    exit()
-
-# WebDriverのインスタンスを生成
-driver = webdriver.Chrome(executable_path='/Users/miyata/dev/UQ_scraping/chromedriver')
-
-time.sleep(1) # ステータスコードの確認から1秒待機
-
-driver.get(url)
-time.sleep(1)  # jsの描画待ち
-
-# element[s]であることに注意
-search_strings = driver.find_elements_by_css_selector('h3.uqv2-parts-text--lg.uqv2-parts-bold')
-
-if len(search_strings) == 0:
-    result = '要素が存在しないためスクレイピングに失敗しました。対象サイトのDOM構造を確認してください。\n{}'.format(url)
-else:
-    if (has_torque_5g()):
-        result = 'TORQUE 5GがUQモバイルの対象機種になりました。契約内容を変更しましょう。\n{}'.format(url)
-
-def has_torque_5g():
-    for tag in search_strings:
-        target_upper = tag.text.upper()
-        if target_upper in 'TORQUE G05' or target_upper in 'TORQUE 5G' or target_upper in 'TORQUE 5G KYG01':
-            return True
-        return False
-
-# ブラウザを閉じる
-driver.quit()
-
-send_email(result)
-
 def send_email(body):
+    """メール送信処理
+
+    Args:
+        body (string): メール本文
+    """
     # SMTPログイン
     smtp_obj = smtplib.SMTP('smtp.gmail.com', 587)
     smtp_obj.ehlo()
@@ -64,3 +34,35 @@ def send_email(body):
 
     smtp_obj.sendmail(msg['From'], msg['To'], msg.as_string())
     smtp_obj.close()
+
+# ステータスコードの確認
+if requests.get(url).status_code != 200:
+    send_email('スクレイピング対象サイトのレスポンスが変更されました。対象サイトを確認してください。\n{}'.format(url))
+    exit()
+
+# WebDriverのインスタンスを生成
+driver = webdriver.Chrome(executable_path='/Users/miyata/dev/UQ_scraping/chromedriver')
+
+time.sleep(1) # ステータスコードの確認から1秒待機
+
+driver.get(url)
+time.sleep(1)  # jsの描画待ち
+
+# element[s]であることに注意
+search_strings = driver.find_elements_by_css_selector('h3.uqv2-parts-text--lg.uqv2-parts-bold')
+
+def has_torque_5g():
+    if len(search_strings) == 0:
+        return '要素が存在しないためスクレイピングに失敗しました。対象サイトのDOM構造を確認してください。\n{}'.format(url)
+    else:
+        for tag in search_strings:
+            target_upper = tag.text.upper()
+            if target_upper in 'TORQUE G05' or target_upper in 'TORQUE 5G' or target_upper in 'TORQUE 5G KYG01':
+                return 'TORQUE 5GがUQモバイルの対象機種になりました。契約内容を変更しましょう。\n{}'.format(url)
+        return 'TORQUE 5GはまだUQモバイルの対象機種ではありません。'
+
+result = has_torque_5g()
+# ブラウザを閉じる
+driver.quit()
+
+send_email(result)
